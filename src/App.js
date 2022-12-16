d133bbf326e4d1198522a7a5a3c3aaa9b8e4fb1a
GameSevenbjj <rueben.beard@gmail.com>
2022-12-16 16:00:09 -0500

    Update App.js
    Added comments to my code

--- src/App.js
+++ src/App.js
@@ -3,22 +3,25 @@ import React, { useEffect, useState } from "react";
 
 
 function App() {
+
+    //Array of colors for the background and I use two methods of math to get a random color
   const backgroundColors = ["purple", "green", "pink", "orange", "yellow", "Aqua", "DarkGrey", "Gold", "LawnGreen", "Lavender", "Ivory", "LightSteelBlue", "MidnightBlue", "SandyBrown" ];
   document.body.style.backgroundColor = backgroundColors[Math.floor(Math.random()*backgroundColors.length)];
   
   
-
+  //react hook to maintain the quotes
   const [quotes, setQuotes] = useState("");
-
+  //fetching the data from the api to get quotes
   const getQuote = () => {
     fetch("https://type.fit/api/quotes")
       .then((res) => res.json())
       .then((data) => {
+        //Randomizes the quote selection
         let randomNum = Math.floor(Math.random() * data.length);
         setQuotes(data[randomNum]);
       });
   };
-
+  //Retrieves a new quote on every refresh
   useEffect(() => {
     getQuote();
   }, []);
